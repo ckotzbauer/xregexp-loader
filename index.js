4847ec59ffbace0848d236e8484c4594dd7d167d
Christian Kotzbauer <christian.kotzbauer@gmail.com>
2017-04-30 15:54:48 +0200

    initial release

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+var xregexp = require("xregexp");
+var _ = require("lodash");
+
 module.exports = function (source, map) {
-    // TODO
+    if (this.cacheable) {
+        this.cacheable();
+    }
+
+    var funcRegex = /XRegExp\((.*)\)/gi;
+
+    var matches = [];
+    var replaced = source;
+    while (matches = funcRegex.exec(source)) {
+        var innerRegex = matches[1].replace(/['"]+/g, '');
+        replaced = replaced.replace(new RegExp(funcRegex), xregexp(innerRegex));
+    }
+
+    this.callback(null, replaced, map);
 };
